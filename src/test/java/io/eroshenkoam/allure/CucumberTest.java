54f057ef07ac3738dc0920d63198e5d0adc08679
Artem Eroshenko <eroshenkoam@me.com>
2019-08-08 12:22:48 +0300

    update plugin settings

--- src/test/java/io/eroshenkoam/allure/CucumberTest.java
+++ src/test/java/io/eroshenkoam/allure/CucumberTest.java
@@ -5,7 +5,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/"})
+@CucumberOptions(
+        monochrome = true,
+        plugin = {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm", "pretty"},
+        features = {"src/test/resources/"}
+)
 public class CucumberTest {
 
 }
